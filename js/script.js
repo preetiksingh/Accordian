90626f3f1797d827e1561492ad47903a638458cf
preetiksingh <preetisingh41451@gmail.com>
2016-01-25 00:54:11 +0530

    Increased transition speed

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $("#accordian p:not(:first)").hide();
   $("#accordian h4").click(function(){
-    $(this).next().slideToggle(1000)
-          .siblings("p:visible").slideUp(1000);
+    $(this).next().slideToggle(500)
+          .siblings("p:visible").slideUp(500);
   });
 });
